4c5ff8a472fe62eec90c306d5db6fb157617d25a
Matthew Wiens <mwiens@cogolabs.com>
2017-12-16 13:10:37 -0500

    9 area dry/wet data set

---
+++ create_9part_dataset.R
@@ -0,0 +1,25 @@
+# Split data into 9 parts
+
+#grid2
+
+#num of sections
+area_count <- 9
+years <- 46
+
+tf_data <- list(rep(0,9))
+for( i in seq(1,area_count)) {
+  for( j in seq(1, years)){
+    #locs <- array(rep(grid2 == i,365),dim = c(16,17,365))
+    locs <- (grid2 == i)
+   this_year <- tf_weather_data[,, seq(j*365-364,j*365)]
+  tf_data[[i]] <- apply(this_year,3,function(x){sum(x&locs,na.rm=T)>(.5*sum(locs)) })
+  }
+}
+
+MrainLevel <- list(rep(0,9))
+for( i in seq(1,9)) {
+MrainLevel[[i]] <- matrix(unlist(lapply(tf_data[[i]],CalculateBounds)),ncol=2,byrow=T)
+write.matrix(MrainLevel[[i]],file=paste("Area",i,"MinMaxData.tsv"),sep="\t")
+}
+
+rain_levels_area <- MrainLevel
